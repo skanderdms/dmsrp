b7618d0c7663e6768d623f13bdada6cc419bb5d2
Mohamed Skander DAAS <45918643+skanderdms@users.noreply.github.com>
2025-08-04 13:44:48 +0100

    Update dmsrp.h

--- dmsrp/doc/dmsrp.h
+++ dmsrp/doc/dmsrp.h
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2023
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software; you can redistribute it and/or modify 
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
  *
